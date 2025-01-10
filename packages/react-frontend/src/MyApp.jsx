cc3fd091683494a777c439ef532d50f91c45541e
Ray Valenzuela <ray.valen04@gmail.com>
2025-01-09 23:10:08 -0800

    switching pcs

---
+++ packages/react-frontend/src/MyApp.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Table from "./Table";
+
+const characters = [
+    {
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ];
+
+export default function MyApp() {
+  return (
+    <div>
+        <Table characterData={characters}/>
+    </div>
+  );
+}
